ef15348484bac0e5ea84b143832324c005179744
Daniel Jader Pellattiero <903837@stud.unive.it>
2025-01-09 02:15:59 +0100

    MongoDB integration.

---
+++ build/main.js
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import express from 'express';
+import { MongoClient } from 'mongodb';
+// Express configuration
+const app = express();
+const port = process.env.PORT || 3000;
+// MongoDB configuration
+const client = new MongoClient('mongodb://localhost:27017/');
+let db;
+// API start up
+app.listen(port, async () => {
+    try {
+        const connection = await client.connect();
+        db = connection.db('unive-sw-arch');
+        console.log(`Server connected to MongoDB at localhost:27017`);
+        console.log(`Server listening at http://localhost:${port}`);
+    }
+    catch (error) {
+        process.exit(1);
+    }
+});
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import 'dotenv/config';
 import express from 'express';
+import {MongoClient} from 'mongodb';
 
+// Express configuration
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
+// MongoDB configuration
+const client = new MongoClient('mongodb://localhost:27017/');
+let db: any;
 
-app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+// API start up
+app.listen(port, async () => {
+  try {
+    const connection = await client.connect();
+    db = connection.db('unive-sw-arch');
+    console.log(`Server connected to MongoDB at localhost:27017`);
+    console.log(`Server listening at http://localhost:${port}`);
+  } catch (error) {
+    process.exit(1);
+  }
 });
